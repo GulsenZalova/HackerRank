1a2c600fe5967560ce363719e635983018b71df8
GulsenZalova <zalova0202@gmail.com>
2022-04-16 04:56:11 +0400

    AlternatingCharacters task done

---
+++ HackerRankTask/AlternatingCharacters.js
@@ -0,0 +1,11 @@
+let s1="AAABBB"
+let say=0
+
+for(let i=0; i<s1.length; i++){
+    if(s1[i]==s1[i+1]){
+        s1[i+1]=0
+        say++
+    }
+}
+console.log(say)
+
--- HackerRankTask/MarkandToys.js
+++ HackerRankTask/MarkandToys.js
@@ -1,13 +1,26 @@
-let k=50
+// let k=50
 // let arr=[1,2,8,3,4,5,9,6,7]
-let arr=[1,12,5,111,200,1000,10]
-let arrsort=arr.sort(function(a,b){return a-b})
-let say=0
-let newarr=[]
+// let arr=[1,12,5,111,200,1000,10]
+// let arrsort=arr.sort(function(a,b){return a-b})
+// let say=0
+// let newarr=[]
 
-for(let i=0; i<arrsort.length; i++){
-    say+=arr[i]
-    if(say<=k)
-   newarr[i]=arrsort[i]
-}
-console.log(newarr.length)
+//  1ci üsul
+
+// for(let i=0; i<arrsort.length; i++){
+//     say+=arr[i]
+//     if(say<=k)
+//    newarr[i]=arrsort[i]
+// }
+// console.log(newarr.length)
+
+// 2ci üsul
+
+// for(let i=0; i<arrsort.length; i++){
+//     k-=arr[i]
+//    if(k<0){
+//    }else{
+//        say++
+//    }
+// }
+// console.log(say)
