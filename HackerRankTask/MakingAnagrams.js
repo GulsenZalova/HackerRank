05b98d7bc4c6e05c4074eaa85d74ee41fc08b786
GulsenZalova <zalova0202@gmail.com>
2022-04-18 01:29:02 +0400

    FibonacciNumbers task done

--- HackerRankTask/MakingAnagrams.js
+++ HackerRankTask/MakingAnagrams.js
@@ -1,52 +1,26 @@
 
-let c ="fcrxzwscanmligyxyvym";
-let d ="jxwtrhvujlmrpdoqbisbwhmgpmeoke";
+// let c ="fcrxzwscanmligyxyvym";
+// let d ="jxwtrhvujlmrpdoqbisbwhmgpmeoke";
  
-function makeAnagram(a, b){
-let say=0;
-let silinen;
-let w=b
-for(let i=0; i<a.length; i++){
-    for(let j=0; j<w.length; j++){
-        if(a[i]==w[j]){
-            say++;
-            w = w.replace(`${w[j]}`, "");
-            break;
-        }
-    }
-}
-silinen=a.length-say + (b.length-say);
-console.log(silinen);
-}
-makeAnagram(c,d)
+// function makeAnagram(a, b){
+// let say=0;
+// let silinen;
+// let w=b
+// for(let i=0; i<a.length; i++){
+//     for(let j=0; j<w.length; j++){
+//         if(a[i]==w[j]){
+//             say++;
+//             w = w.replace(`${w[j]}`, "");
+//             break;
+//         }
+//     }
+// }
+// silinen=a.length-say + (b.length-say);
+// console.log(silinen);
+// }
+// makeAnagram(c,d)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//  Düzəliş edilməli olan üsuldur.Mütləq bax.
 
 // let a="fcrxzwscanmligyxyvym";
 // let b="jxwtrhvujlmrpdoqbisbwhmgpmeoke";
@@ -75,25 +49,4 @@ makeAnagram(c,d)
 
 
 
-// for(let i of a){
-//     if(b.includes(i)){
-//     }else{
-//         c+=i
-//     }
-// }
 
-// for(let i of b){
-//     if(a.includes(i)){
-//     }else{
-//         c+=i
-//     }
-    
-// }
-// console.log(c.length)
-
-// for(let i=0; i<a.length; i++){
-//     if(a[i]===a[i+1]){
-//         a[i+1]=0
-//         console.log(a[i])
-//     }
-// }
---
+++ HackerRankTask/RecursionAndBacktracking.js
@@ -0,0 +1,9 @@
+let arr=[]
+let n=4
+
+for(let i=2; i<=n; i++){
+    arr.push(0)
+    arr.push(1)
+    arr[i]=arr[i-1]+arr[i-2]
+}
+console.log(arr[n])
